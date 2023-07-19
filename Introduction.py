926c3882cf57974d4b023a6223118b326c667108
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2023-07-19 10:31:57 +0200

    update introduction

---
+++ Introduction.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 19 10:27:34 2023
+
+@author: namnguyen
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import base64
+from io import BytesIO
+import All_functions as AF
+
+st.title("Aerodynamic")
+
+
+
+st.write('Hola')
+
+
+
+
+
+st.markdown("""
+---
+- The program developed by: 		Pedram Manouchehri & Nam Nguyen 
+- User interface developed by:	Nam Nguyen 
+- Independently Checked by:		
+""")
