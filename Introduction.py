8ec5309595ac830787c94a1fb6b9cdad9a018868
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2023-07-20 11:49:55 +0200

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -15,7 +15,7 @@ import base64
 from io import BytesIO
 import Aerodynamic_Excitation as AF
 
-st.title("Aerodynamic")
+st.title(" DESIGN RULES FOR AERODYNAMIC EFFECTS ON BRIDGES")
 
 
 st.markdown("""
