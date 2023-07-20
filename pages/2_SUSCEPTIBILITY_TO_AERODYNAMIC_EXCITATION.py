a5e809a13e1cc2adf3acdcf9d4bdeb6c05805adc
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2023-07-20 10:09:15 +0200

    w

--- pages/2_SUSCEPTIBILITY_TO_AERODYNAMIC_EXCITATION.py
+++ pages/2_SUSCEPTIBILITY_TO_AERODYNAMIC_EXCITATION.py
@@ -184,7 +184,7 @@ if P_b>0.04 and P_b<1.00:
 	st.latex(V_WO)
 	V_WO=latex(AF.V_WO_func(V_r=V_r, V_d=V_d, K_1A=K_1A))
 	st.latex(V_WO)
-	V_WO=round_equation((AF.V_WO_func(V_r=V_r, V_d=V_d, K_1A=K_1A).doit()),2)
+	V_WO=AF.round_equation((AF.V_WO_func(V_r=V_r, V_d=V_d, K_1A=K_1A).doit()),2)
 	st.latex(latex(V_WO))
 	
 if 	P_b>=1:
