24e04a49c6cab689ee219b006bc65190753707ae
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2023-07-27 08:59:26 +0200

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -17,19 +17,17 @@ import Aerodynamic_Excitation as AF
 
 st.header(" DESIGN RULES FOR AERODYNAMIC EFFECTS ON BRIDGES")
 
-
 st.markdown("""
 
 This Standard specifies design requirements for bridges regarding aerodynamic effects, applicable to highway bridges and foot/cycle-track bridges meeting outlined constraints. Mandatory requirements are highlighted within boxes, while the document also includes advisory content for consideration. Overall, the Standard aims to enhance aerodynamic performance and safety of bridges, ensuring efficiency and reliability.
 
 """)
 
-st.markdown("[Click here to see PDF document](https://github.com/CFCSL/AERODYNAMIC-EFFECTS-ON-BRIDGES/blob/main/bd4901.pdf)")
+st.markdown("[Click here to see PDF document](https://github.com/FCSL/AERODYNAMIC-EFFECTS-ON-BRIDGES/blob/main/bd4901.pdf)")
 
 st.write("The calculation files in Jupyter Notebook and Python  are available in GitHub")
 st.markdown("[Jupyter Notebook document](https://github.com/CFCSL/AERODYNAMIC-EFFECTS-ON-BRIDGES/blob/main/Aerodynamic_Excitation-Copy1.ipynb)")
 st.markdown("[Python document](https://github.com/CFCSL/AERODYNAMIC-EFFECTS-ON-BRIDGES/blob/main/Aerodynamic_Excitation.py)")
-
 
 
 
