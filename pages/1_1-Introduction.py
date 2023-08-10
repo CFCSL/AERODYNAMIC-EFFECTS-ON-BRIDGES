84476df41af6be4e19b190b9f23dd970eb61fc2a
PedramManouchehri <135531479+PedramManouchehri@users.noreply.github.com>
2023-08-10 23:27:53 +0200

    Update 1_1-Introduction.py

--- pages/1_1-Introduction.py
+++ pages/1_1-Introduction.py
@@ -4,8 +4,8 @@ import streamlit as st
 st.header("1.Scope")
 st.markdown("""This Standard specifies design requirements for bridges with respect to aerodynamic effects,
 including provisions for wind-tunnel testing. It supersedes clause 5.3.9 of BS 5400: Part 2(1) and the previous version of this standard BD 49/93.
-All references to BS 5400: Part 2 are intended to imply the document as implemented by BD 37(2) (DMRB 1.3).""")
-This Standard is applicable to all highway bridges and foot/cycle-track bridges. However its provisions shall only apply to bridges which comply with the constraints outlined herein.
+All references to BS 5400: Part 2 are intended to imply the document as implemented by BD 37(2) (DMRB 1.3). This Standard is applicable to all highway bridges and foot/cycle-track bridges. However its provisions shall only apply to bridges which comply with the constraints outlined herein.""")
+
 st.subheader("1.2 Design requirements")
 st.markdown("""The aerodynamic aspects of bridge design shall be carried out in accordance with the rules including associated annexes when applicable.
 The requirements, in the form of design rules, are given in the following sections. Proximity effects are covered in Annex A. Formulae for the prediction of fundamental frequencies in bending and in torsion are given in Annex B, and further requirements for wind tunnel testing are given in Annex C.""")
